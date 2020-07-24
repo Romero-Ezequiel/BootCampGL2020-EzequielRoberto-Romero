112fc31c1e8510805bc392381bd79a04695b7ebd
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-07-24 06:48:39 -0300

    Ejercicio 4.4 - Módulo 1 - Introducción

---
+++ Modulo-001/Ejercicio4-4/src/main/java/ControlDeFlujosFor/Main.java
@@ -0,0 +1,26 @@
+package ControlDeFlujosFor;
+
+/**
+Ejercicio 4.4:  Realizar un for de 1 a 10, imprimiendo el valor iterado
+Pasos:
+Colocar la cláusula For, valor inicial i=1, valor final <=10, y que 
+incremente en 1. Imprimer mediante System.out.println el siguiente mensaje 
+“Valor iterado: ” concatenado con el valor de i
+
+Ejemplo salida: 
+Valor iterado: 1
+Valor iterado: 2
+...
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+
+        for(int i=1; i <= 10; i++ ){
+            System.out.println("Valor iterado es : "+ i);
+        }
+
+    }
+}
