24eb2514a62b7a7448a08a45d1aa5e19482a90e5
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-07-24 09:07:54 -0300

    Ejercicio 5.1 - Módulo 1 - Introducción a Herencias

---
+++ Modulo-001/Ejercicio5-1/src/main/java/POO/App.java
@@ -0,0 +1,21 @@
+package POO;
+
+/**
+Ejercicio 5.1: Herencia
+Crear clases: Electrodoméstico y Heladera que se muestran en el diagrama.
+Hacer que Heladera extienda de Electrodoméstico.
+A la hora de implementar los métodos hacerlo imprimiendo una descripción de
+su comportamiento. Ej: System.out.println("Encendiendo electrodoméstico")
+Crear una instancia de Electrodoméstico y una de Heladera.
+Imprimir atributos y métodos de las instancias de Electrodoméstico y Heladera.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Electrodomestico heladera2020 = new Electrodomestico();
+        heladera2020.encender();
+        //System.out.println( "Hello World!" );
+    }
+}
