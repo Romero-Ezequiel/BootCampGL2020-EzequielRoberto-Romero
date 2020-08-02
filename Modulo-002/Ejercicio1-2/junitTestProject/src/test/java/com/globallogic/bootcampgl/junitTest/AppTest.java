cb13f8bf4e1a56b6d2b92c781453b74e1f866f0f
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-08-02 20:11:37 -0300

    Ejercicio 1.2 - Módulo 2 - Introducción a JUnit 5 y assertions

--- Modulo-002/Ejercicio1-2/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
+++ Modulo-002/Ejercicio1-2/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
@@ -6,6 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
     
+    final String cadenaTexto1 = "lalala";
+    final String cadenaTexto2 = "lalala";
+    final String cadenaTexto3 = "lololo";
+
+    @Test
+    void test() {
+        assertTrue(cadenaTexto1.equals(cadenaTexto2) && !cadenaTexto2.equals(cadenaTexto3));
+    }
+    
+    /*
+    
     @Test
     void testString() {
         String cadenaTexto1 = "lalala";
@@ -13,6 +24,6 @@ class AppTest {
         String cadenaTexto3 = "lololo";
         
         assertNotEquals(cadenaTexto1, cadenaTexto2, cadenaTexto3);
-    }
+    }*/
 
 }
