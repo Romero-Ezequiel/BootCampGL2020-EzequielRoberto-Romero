e665338c174dc205caa8ffcc4dd22bb38b745817
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-08-05 05:50:14 -0300

    Ejercicio 1.2 - Módulo 3 - Patrón Singleton

---
+++ Modulo-003/Ejercicio1.2/src/main/java/com/globallogic/bootcamp/patterns/App.java
@@ -0,0 +1,12 @@
+package com.globallogic.bootcamp.patterns;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Currency c = Currency.getInstance();
+        c.mostrar();
+   
+    }
+    
+}
