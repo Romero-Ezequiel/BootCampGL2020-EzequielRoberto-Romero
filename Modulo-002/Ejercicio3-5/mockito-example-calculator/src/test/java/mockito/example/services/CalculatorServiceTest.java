6fb05fcfe0ffd737f3fa529d83548ce8f28b6bc1
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-08-06 18:28:18 -0300

    Ejercicio 3.5 - Módulo 2 - Mockito

---
+++ Modulo-002/Ejercicio3-5/mockito-example-calculator/src/test/java/mockito/example/services/CalculatorServiceTest.java
@@ -0,0 +1,33 @@
+package mockito.example.services;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import mockito.example.services.impl.CalculatorServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculatorServiceTest {
+
+    @InjectMocks
+    private CalculatorServiceImpl calculatorServiceImpl;
+
+    @Mock
+    DataService dataService;
+	
+    @Test
+    @DisplayName("Test NullPointerException")
+    void tesNullPointerException() {
+
+	when(dataService.getListOfNumbers()).thenReturn(null);
+        assertThrows(NullPointerException.class, () -> {
+            calculatorServiceImpl.calculateAverage();
+        });
+    }
+}
