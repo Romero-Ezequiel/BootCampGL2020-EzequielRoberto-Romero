811a543b858042227d9da8b4349b0fb2436775f7
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-08-06 15:17:32 -0300

    Ejercicio 3.3 - Módulo 2 - Mockito

---
+++ Modulo-002/Ejercicio3-3/mockito-example-calculator/src/main/java/mockito/example/services/CalculatorService.java
@@ -0,0 +1,12 @@
+package mockito.example.services;
+
+public interface CalculatorService {
+
+    double calculateSum(int a, int b);
+    double calculateSubstraction(int a, int b);
+    double calculateDivision(int a, int b);
+    double calculateMultiplication(int a, int b);
+    double calculateAverage();
+    boolean isOdd(int a);
+    String printResult(double result);
+}
