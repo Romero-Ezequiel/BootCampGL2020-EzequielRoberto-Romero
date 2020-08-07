4cf27bff93343f1f080946f50c0abe17da37e6ee
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-08-06 22:42:34 -0300

    Ejercicio 3.7 - Módulo 2 - Mockito

---
+++ Modulo-002/Ejercicio3-7/mockito-example-calculator/src/main/java/mockito/example/exception/ZeroDivisionException.java
@@ -0,0 +1,12 @@
+package mockito.example.exception;
+
+public class ZeroDivisionException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ZeroDivisionException() {
+		
+    
+    }
+    
+}
---
+++ Modulo-002/Ejercicio3-7/mockito-example-calculator/src/test/java/mockito/example/services/CalculatorServiceTest.java
@@ -0,0 +1,30 @@
+package mockito.example.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import mockito.example.services.impl.CalculatorServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculatorServiceTest {
+
+    @InjectMocks
+    private CalculatorServiceImpl calculatorServiceImpl;
+
+    @Mock
+    private BasicOperationsService basicOperationsService;
+
+    @Test
+    @DisplayName("Ejercicio 3.7")
+    void testSuma() {
+        
+        when(basicOperationsService.add(2, 3)).thenReturn(5.0);
+        assertEquals("El resultado es: " + calculatorServiceImpl.calculateSum(2, 3), calculatorServiceImpl.printResult(calculatorServiceImpl.calculateSum(2, 3)));
+    
+    }
+}
