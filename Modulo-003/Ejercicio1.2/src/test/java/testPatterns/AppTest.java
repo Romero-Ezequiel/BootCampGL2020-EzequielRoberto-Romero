ad47169839ddecbc2af0e7c87e424de2d4771ce5
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-08-09 16:01:37 -0300

    Ejercicio 1.2 - Módulo 3 - Patrón Singleton

--- Modulo-003/Ejercicio1.2/src/test/java/testPatterns/AppTest.java
+++ Modulo-003/Ejercicio1.2/src/test/java/testPatterns/AppTest.java
@@ -2,9 +2,7 @@ package testPatterns;
 
 //import java.util.Currency;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 
 public class AppTest {
     
